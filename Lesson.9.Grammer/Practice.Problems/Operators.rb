fd43c4dbaad63527c804aa99fc8ecb683493729c
qqdipps <sdipp007@gmail.com>
2018-09-17 14:49:52 -0700

    Ready for test

--- Lesson.9.Grammer/Practice.Problems/Operators.rb
+++ Lesson.9.Grammer/Practice.Problems/Operators.rb
@@ -27,26 +27,127 @@ puts "result answer is correct: #{s==q}"
 puts type + type2
 
 s = d - f	
-puts "d + f	"
-q = 12.0
+puts "d - f	"
+q = 8
 puts "answer: #{q}" 
 puts "result answer is correct: #{s==q}" 	
 puts type + type1
 
 s = g - e		
+puts "g + e	"
+q = 6.0
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type2
+
 s = (h + i) - f		
-s = (d - f) + e		
+puts "(h + i) - f"
+q = 2.5
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type2
+
+s = (d - f) + e	
+puts "(d - f) + e	"
+q = 13.0
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type2
+
 s = d * f		
+puts "d * f	"
+q = 20
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type1
+
 s = g * i		
+puts "g * i	"
+q = 16.5
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type2
+
 s = f * g		
+puts "f * g	"
+q = 22.0
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type2
+
 s = d / f		
-s = d / e		
+puts "d / f	"
+q = 5
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type1
+
+s = d / e
+puts "d / e	"
+q = 2.0
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type2
+
 s = e / f		
+puts "e / f	"
+q = 2.5
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type2
+
 s = (g * f) / f		
+puts "(g * f) / f"
+q = 11.0
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type2
+
 s = (d / f) * e		
+puts "(d / f) * e"
+q =  25.0
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type2
+
 s = 21 / 5		
+puts "21 / 5	"
+q = 4
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type1
+
 s = 14 / 5		
+puts "14 /5	"
+q = 2
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type1
+
 s = 10 % 3		
+puts "10 % 3	"
+q = 1
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type1
+
 s = 20 % 2		
+puts "20 % 2	"
+q = 0
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type1
+
 s = 4 % 5		
+puts "4 % 5	"
+q = 4
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type1
+
 s = 8 % 1	
+puts "8 % 1"
+q = 0
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type1
