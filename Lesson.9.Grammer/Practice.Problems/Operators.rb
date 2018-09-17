c3d49b71d8ba68e138ac6271c6dae60b42c143a1
qqdipps <sdipp007@gmail.com>
2018-09-17 14:47:47 -0700

    Operators.txt- contains answers and check statements Operators.rb- working file to check

---
+++ Lesson.9.Grammer/Practice.Problems/Operators.rb
@@ -0,0 +1,52 @@
+d = 10
+e = 5.0
+f = 2
+g = 11.0
+h = 3
+i = 1.5
+#Operation
+#Result	
+#DataType
+type = "Data type of result:"
+type1 = " fixed num"	
+type2 = "float"
+
+
+s = f + h				
+q = 5 
+puts "f + h	"
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type1
+
+s = g + h	
+puts "g + h	"
+q = 14.0
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type2
+
+s = d - f	
+puts "d + f	"
+q = 12.0
+puts "answer: #{q}" 
+puts "result answer is correct: #{s==q}" 	
+puts type + type1
+
+s = g - e		
+s = (h + i) - f		
+s = (d - f) + e		
+s = d * f		
+s = g * i		
+s = f * g		
+s = d / f		
+s = d / e		
+s = e / f		
+s = (g * f) / f		
+s = (d / f) * e		
+s = 21 / 5		
+s = 14 / 5		
+s = 10 % 3		
+s = 20 % 2		
+s = 4 % 5		
+s = 8 % 1	
