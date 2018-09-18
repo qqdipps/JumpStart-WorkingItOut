a42c1f05ea69da496b451a0e792e8dec52bb8200
qqdipps <sdipp007@gmail.com>
2018-09-18 15:54:10 -0700

    Added logic for mm selcetion to determine if user has enough money to purchase selecteditem. Returns statment for scenarios 1. enough money 2. not enough money, but enough for something 3. not enough money for anything 4. no money (0.00).
    Added formatting for floats.
    
    Included test case console output at end of file.

--- Lesson.10.Expressions/CandyMachine.rb
+++ Lesson.10.Expressions/CandyMachine.rb
@@ -29,13 +29,16 @@ puts "Welcome to Virtual Candy Machine!"
 puts"" #format
 
 #user prompts for $
-print "How much money do you have? $"
+print "How much money do you have? $ "
 #user input for $
 moneyUser = gets.chomp.to_f #float (decimal)
+puts"" #format
 
-puts "Great! You have $#{moneyUser}, 
+puts "Great! You have $ %0.2f" % [moneyUser] << ", 
 Let's see what we can do for you..."
 #each candy -> 1 row as string 
+
+puts"" #format
 
 #M&M 
 mm = "A $0.75 M&M"
@@ -60,23 +63,155 @@ puts ah
 puts ss
 puts fr
 
+puts"" #format
+
 #user prompts for selection
 print "What tickles your fancy?: "
 #user input for selection
 candyUser = gets.chomp.upcase
 
+puts"" #format
+
 #user input selection compared to candy string to find selection
-if <candyUser == mm.slice(0)>
-	print mm
-if <candyUser == tw.slice(0)>
+if candyUser == mm.slice(0)
+	#set generic variable for easy code re-use
+	candy = mm
+	#check if user has enough money for candy selected
+	if moneyUser >= candy.slice(3,4).to_f
+		change = moneyUser - candy.slice(3,4).to_f
+		#yes calculate change and return statement
+		puts "beeboobeeep... enjoy your " << candy.slice(8..-1) << "!"
+		puts"tingchchch... don't forget your change: $ %0.2f" % [change]
+	elsif moneyUser > 0
+		short = candy.slice(3,4).to_f - moneyUser
+		puts "Sorry! you don't have enough for " << candy.slice(8..-1) << "."
+		puts "You're short $ %0.2f" % [short] << ", but you may have enough for..."
+		
+		if moneyUser >= mm.slice(3,4).to_f 
+			puts mm
+		end
+		if moneyUser >= tw.slice(3,4).to_f 
+			puts tw
+		end
+		if moneyUser >= ah.slice(3,4).to_f 
+			puts ah
+		end
+		if moneyUser >= ss.slice(3,4).to_f 
+			puts ss
+		end
+		if moneyUser >= fr.slice(3,4).to_f 
+			puts fr
+		else
+			puts "nothing, you have enough for nothing. Sorry."
+		end
+	else
+		puts "Hey no money was inserted!"
+	end	
+end
+
+if candyUser == tw.slice(0)
 	print tw
-if <candyUser == ah.slice(0)>
+end
+
+if candyUser == ah.slice(0)
 	print ah
-if <candyUser == ss.slice(0)>
+end
+
+if candyUser == ss.slice(0)
 	print ss
-if <candyUser == fr.slice(0)>
+end
+
+if candyUser == fr.slice(0)
 	print fr
-#$ input compared to $ of candy selected.
-# check if $ input > $ candy
-# if yes calc change
-# if no message no
+end 
+
+=begin
+
+4 test cases
+---------------
+1. enough money
+2. not enough money, but enough for something
+3. not enough money for anything
+4. no money (0.00)
+
+{18-09-18 15:46}Savannahs-MBP:~/AdaJumpStart/Lesson.10.Expressions@master✗✗✗✗✗✗ qqdipps% ruby CandyMachine.rb
+Welcome to Virtual Candy Machine!
+
+How much money do you have? $.9
+
+Great! You have $ 0.90,
+Let's see what we can do for you...
+
+Today we have:
+A $0.75 M&M
+B $1.00 Twix
+C $0.35 Air Head
+D $1.25 Sour Skittles
+E $0.55 Fruit Roll-Up
+
+What tickles your fancy?: A
+
+beeboobeeep... enjoy your M&M!
+tingchchch... don't forget your change: $ 0.15
+{18-09-18 15:46}Savannahs-MBP:~/AdaJumpStart/Lesson.10.Expressions@master✗✗✗✗✗✗ qqdipps% ruby CandyMachine.rb
+Welcome to Virtual Candy Machine!
+
+How much money do you have? $0.60
+
+Great! You have $ 0.60,
+Let's see what we can do for you...
+
+Today we have:
+A $0.75 M&M
+B $1.00 Twix
+C $0.35 Air Head
+D $1.25 Sour Skittles
+E $0.55 Fruit Roll-Up
+
+What tickles your fancy?: A
+
+Sorry! you don't have enough for M&M.
+You're short $ 0.15, but you may have enough for...
+C $0.35 Air Head
+E $0.55 Fruit Roll-Up
+{18-09-18 15:47}Savannahs-MBP:~/AdaJumpStart/Lesson.10.Expressions@master✗✗✗✗✗✗ qqdipps% ruby CandyMachine.rb
+Welcome to Virtual Candy Machine!
+
+How much money do you have? $0.20
+
+Great! You have $ 0.20,
+Let's see what we can do for you...
+
+Today we have:
+A $0.75 M&M
+B $1.00 Twix
+C $0.35 Air Head
+D $1.25 Sour Skittles
+E $0.55 Fruit Roll-Up
+
+What tickles your fancy?: A
+
+Sorry! you don't have enough for M&M.
+You're short $ 0.55, but you may have enough for...
+nothing, you have enough for nothing. Sorry.
+{18-09-18 15:48}Savannahs-MBP:~/AdaJumpStart/Lesson.10.Expressions@master✗✗✗✗✗✗ qqdipps% ruby CandyMachine.rb
+Welcome to Virtual Candy Machine!
+
+How much money do you have? $ 0
+
+Great! You have $ 0.00,
+Let's see what we can do for you...
+
+Today we have:
+A $0.75 M&M
+B $1.00 Twix
+C $0.35 Air Head
+D $1.25 Sour Skittles
+E $0.55 Fruit Roll-Up
+
+What tickles your fancy?: A
+
+Hey no money was inserted!	
+
+	
+=end
