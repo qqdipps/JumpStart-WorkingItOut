c8a28c4da1e0445fccc28ed10434fd176a5d87a7
qqdipps <sdipp007@gmail.com>
2018-09-18 13:52:07 -0700

    Outline

---
+++ Lesson.10.Expressions/CandyMachine.rb
@@ -0,0 +1,43 @@
+=begin
+Overview
+This challenge is to build a computer candy machine.
+ You’ve got some money and you want to buy some candy. 
+ So, you go up to the candy machine, put in your money, 
+ select your candy, and then pick up your candy and your 
+ change. You can use gets.chomp to get input from the 
+ user without the extra newline at the end.
+
+Requirements
+Ask the user how much money they have, assume that the $ 
+symbol is part of the prompt (the user doesn't have to enter it)
+
+Display all candy options and their costs (even if the user cannot 
+afford the candy)
+
+Decide whether the user can afford the candy or not, if they can't,
+tell them so, if they can, calculate and display their change
+
+[Optional] Handle when the buyer enters "C" or "c" so that it works
+ as expected
+
+[Optional] Do something appropriate when the buyer
+enters an invalid amount for the money and an invalid selection
+end
+
+#Opening statment
+#user prompts for $
+#user input for $
+#display candy
+#each candy -> 1 row as string 
+#M&M 
+#twix
+#airheads
+#sour skittles
+#fruit roll-up
+#user prompts for selection
+#user input for selection
+#user input selection compared to candy string to find selection
+#$ input compared to $ of candy selected.
+# check if $ input > $ candy
+# if yes calc change
+# if no message no
