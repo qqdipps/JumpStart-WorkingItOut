db7d9b0f2e1b9d6e67bfe722d62d0e22bf4031a9
qqdipps <sdipp007@gmail.com>
2018-09-19 00:56:34 -0700

    Learned why 'num.to_f.to_s ==  num' will check if num is float. Used logic in CandyMachine.rb

--- Lesson.10.Expressions/formatNum.rb
+++ Lesson.10.Expressions/formatNum.rb
@@ -1,3 +1,10 @@
 score = 78.5431
 puts "The average is %0.2f" % [score]
 
+
+num = gets.chomp
+if num.to_f.to_s ==  num #yes i get it, was puzzled but now I get it! when converted to float letters get dropped or set to 0 so when its turned back to string and compared to orginal it will be different if letters are there.
+	print num
+else
+	puts "not"
+end
