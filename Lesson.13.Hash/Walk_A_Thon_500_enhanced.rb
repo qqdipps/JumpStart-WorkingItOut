3605f17a489246fba378909e13c4921a81d1a22e
qqdipps <sdipp007@gmail.com>
2018-10-09 01:25:55 -0700

    Adpated logic to account for new data format. Updated test cases.

--- Lesson.13.Hash/Walk_A_Thon_500_enhanced.rb
+++ Lesson.13.Hash/Walk_A_Thon_500_enhanced.rb
@@ -73,120 +73,116 @@ roster.each(walker_ID: 'walker_ID', laps_walked: 'laps_walked', amount_per_lap: 
   master_roster << hash
 end
 
-# winner = []
+# Adds hash that stores key (walker id number) with value (amount raised) to tracker.
+master_roster.each do |participant_data|
+	if participant_data[:walker_id] != "walker_ID"
+		tracker["##{participant_data[:walker_ID]}"] = (participant_data[:laps_walked].to_i * participant_data[:amount_per_lap].to_f)
+	end
+end
 
-# # Adds walker number to winner array if laps is greatest. Works for ties.
-# tracker.each {|walker, laps| winner << walker if laps == tracker.values.max}
+winner = []
 
-# # Calculates tbe amount of money winner walkers have earned based on the amount of laps and amount walked.
-# # Prints formatted list of winners w/ correct grammer.
-# # WINNER 1 has ..
-# # WINNER 1 and WINNER 2 have ...
-# # WINNER 1, WINNER 2, WINNER n-1, and WINNER n have ....
-# puts "\n\nWhile there are no winners in a Walk-A-Thon. \nWe would like to take a moment and acknowledge those who \ncontributed the time and energy to walk the talk:"
-# print "\nWalker "
-# winner.each do |walker|
-# 	# If there are 1 or 2 winners then the first walker should only be participant's name & space.
-# 	if winner.length() <=2 &&  walker == winner[0]
-# 		print "#{walker} "
-# 		# If there is only 1 winner then singular winner comment is appended.
-# 		print (winner.length() == 1	? "has " : "")
-# 	# If there is 3 or more winners then each walker is printed with participant's name, comma, & space.
-# 	# Unless walker is the last winner then it is handled by else statement. 
-# 	elsif walker != winner[-1]
-# 		print "#{walker}, "
-# 	# If there is more than one walker the last walker is 'and', space, participant's name, space, & pluaral winning comment.
-# 	else
-# 		print "and #{walker} have each "
-# 	end
-# end
-# print "earned the most money, $%0.2f by walking #{tracker[winner[0]]} laps" % [(tracker[winner[0]] * amount)]
-# puts winner.length() > 1? " a piece." : "."
+# Adds walker number to winner array if amount raised is max. Works for ties.
+tracker.each {|walker, amount_raised| winner << walker if amount_raised == tracker.values.max}
 
-# # Walk-A-Thon Results:
-# puts "\n\nUnited Walk-A-Thon Results:"
-# puts "=========================\n\n"
-# # Have the walk-a-thon program calculate and output the following:
-# # Who earned the most money, which keys are max values?
-# # Total amount earned in the walk-a-thon.
-# total_earned =  tracker.values.inject(:+) * amount
-# puts "Total raised was $%0.2f" % [total_earned]
-# # Whether or not the earning goal was met.
-# # Is total earned greater than goal?
-# puts total_earned > goal ? "Congratulations! Our goal was met! \nWe surpassed our goal by $%0.2f!" % [total_earned - goal] : "We fell short by $%0.2f, let's try another fundraiser to make our goal!" % [goal - total_earned] + "\n\n"
+# Calculates tbe amount of money winner walkers have earned based on the amount of laps and amount walked.
+# Prints formatted list of winners w/ correct grammer.
+# WINNER 1 has ..
+# WINNER 1 and WINNER 2 have ...
+# WINNER 1, WINNER 2, WINNER n-1, and WINNER n have ....
+puts "\n\nWhile there are no winners in a Walk-A-Thon. \nWe would like to take a moment and acknowledge those who \ncontributed the time and energy to walk the talk:"
+print "\nWalker "
+winner.each do |walker|
+	# If there are 1 or 2 winners then the first walker should only be participant's name & space.
+	if winner.length() <= 2 && walker == winner[0]
+		print "#{walker} "
+		# If there is only 1 winner then singular winner comment is appended.
+		print (winner.length() == 1	? "has " : "")
+	# If there is 3 or more winners then each walker is printed with participant's name, comma, & space.
+	# Unless walker is the last winner then it is handled by else statement. 
+	elsif walker != winner[-1]
+		print "#{walker}, "
+	# If there is more than one walker the last walker is 'and', space, participant's name, space, & pluaral winning comment.
+	else
+		print "and #{walker} have each "
+	end
+end
+print "earned the most money, $%0.2f" % [tracker[winner[0]]]
+puts winner.length() > 1 ? " a piece." : "."
 
-# =begin 
-# CONSOLE
-# Test cases:
-# 1. Goal not met, with 4 way tie
-# 2. Goal met, single winner.
+# Walk-A-Thon Results:
+puts "\n\nUnited Walk-A-Thon Results:"
+puts "=========================\n\n"
+# Have the walk-a-thon program calculate and output the following:
+# Who earned the most money, which keys are max values?
+# Total amount earned in the walk-a-thon.
+total_earned =  tracker.values.inject(:+)
+puts "Total raised was $%0.2f" % [total_earned]
+# Whether or not the earning goal was met.
+# Is total earned greater than goal?
+puts total_earned > goal ? "Congratulations! Our goal was met! \nWe surpassed our goal by $%0.2f!" % [total_earned - goal] : "We fell short by $%0.2f, let's try another fundraiser to make our goal!" % [goal - total_earned] + "\n\n"
 
-# ------------TEST 1------------
-# {18-10-08 21:51}[ruby-2.4.1]Savannahs-MacBook-Pro:~/AdaJumpStart/Lesson.13.Hash@master✗✗✗✗✗✗ qqdipps% ruby Walk_A_Thon.rb
+=begin 
+CONSOLE
+Test cases:
+1. Goal met
+2. Goal not met
 
-# Welcome to the United Walk-A-Thon!
-# Our goal is to raise money to help fund programs to
-# support people who are targets of systemic oppression.
+------------TEST 1------------
+{18-10-09 1:05}[ruby-2.4.1]Savannahs-MacBook-Pro:~/AdaJumpStart/Lesson.13.Hash@master✗✗✗✗✗✗ qqdipps% ruby Walk_A_Thon_500_enhanced.rb
 
-
-# TRACKER
-# =========================
-
-# Goal: $ 500
-# Amount earned per lap completed (per person): $ 5
-# Number of laps walked per walker?
-# Walker #1: 10
-# Walker #2: 10
-# Walker #3: 10
-# Walker #4: 10
-# Walker #5: 8
+Updated data for second test case.
+Welcome to the United Walk-A-Thon!
+Our goal is to raise money to help fund programs to
+support people who are targets of systemic oppression.
 
 
-# While there are no winners in a Walk-A-Thon.
-# We would like to take a moment and acknowledge those who
-# contributed the time and energy to walk the talk:
+TRACKER
+=========================
 
-# Walker #1, #2, #3, and #4 have each earned the most money, $50.00 by walking 10 laps a piece.
-
-
-# United Walk-A-Thon Results:
-# =========================
-
-# Total raised was $240.00
-# We fell short by $260.00, let's try another fundraiser to make our goal!
-
-# ---------------TEST 2------------
-# {18-10-08 21:53}[ruby-2.4.1]Savannahs-MacBook-Pro:~/AdaJumpStart/Lesson.13.Hash@master✗✗✗✗✗✗ qqdipps% ruby Walk_A_Thon.rb
-
-# Welcome to the United Walk-A-Thon!
-# Our goal is to raise money to help fund programs to
-# support people who are targets of systemic oppression.
+Goal: $ 25000
 
 
-# TRACKER
-# =========================
+While there are no winners in a Walk-A-Thon.
+We would like to take a moment and acknowledge those who
+contributed the time and energy to walk the talk:
 
-# Goal: $ 260
-# Amount earned per lap completed (per person): $ 2.35
-# Number of laps walked per walker?
-# Walker #1: 88
-# Walker #2: 67
-# Walker #3: 45
-# Walker #4: 24
-# Walker #5: 122
+Walker #38, #198, and #436 have each earned the most money, $200.00 a piece.
 
 
-# While there are no winners in a Walk-A-Thon.
-# We would like to take a moment and acknowledge those who
-# contributed the time and energy to walk the talk:
+United Walk-A-Thon Results:
+=========================
 
-# Walker #5 has earned the most money, $286.70 by walking 122 laps.
+Total raised was $25023.00
+Congratulations! Our goal was met!
+We surpassed our goal by $23.00!
 
 
-# United Walk-A-Thon Results:
-# =========================
+---------------TEST 2---------
+{18-10-09 1:07}[ruby-2.4.1]Savannahs-MacBook-Pro:~/AdaJumpStart/Lesson.13.Hash@master✗✗✗✗✗✗ qqdipps% ruby Walk_A_Thon_500_enhanced.rb
 
-# Total raised was $813.10
-# Congratulations! Our goal was met!
-# We surpassed our goal by $553.10!
-# =end
+Welcome to the United Walk-A-Thon!
+Our goal is to raise money to help fund programs to
+support people who are targets of systemic oppression.
+
+
+TRACKER
+=========================
+
+Goal: $ 30000
+
+
+While there are no winners in a Walk-A-Thon.
+We would like to take a moment and acknowledge those who
+contributed the time and energy to walk the talk:
+
+Walker #26, #133, #298, and #410 have each earned the most money, $190.00 a piece.
+
+
+United Walk-A-Thon Results:
+=========================
+
+Total raised was $25303.00
+We fell short by $4697.00, let's try another fundraiser to make our goal!
+
+=end
